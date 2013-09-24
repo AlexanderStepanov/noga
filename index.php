e4f91c50bcd08b43abac0f292c44ecc6422aee31
felicita <alex.stepanov@me.com>
2013-09-24 18:55:15 +0300

    first screen ui

--- index.php
+++ index.php
@@ -9,6 +9,99 @@
         Noga Landing
     </title>
 
+    <style>
+        * {margin: 0; padding: 0;}
+
+        body{
+            margin: 0;
+            padding: 0;
+            height: 100%;
+        }
+
+        html {
+            margin: 0; padding: 0;
+        }
+
+        ::-webkit-input-placeholder { /* WebKit browsers */
+            color:    white;
+            opacity: 0.7 !important;
+        }
+
+        #valid{
+            position: absolute;
+            margin-top: 50px;
+            margin-left: 30px;
+            color: white;
+        }
+
+        #first{
+            margin: 0;
+            margin-top: 0px;
+            height: 100%;
+            width: 100%;
+            position: absolute;
+            min-height: 100%;
+            background-image: url("/img/backgroud.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+
+        #second{
+            height: 100%;
+            width: 100%;
+            position: absolute;
+            min-height: 100%;
+            top: 100%;
+            background-color: white;
+        }
+        
+        #emailDiv{
+            background-image: url("/img/inputRoundedBackground.png");
+            padding-top: 10px;
+            padding-left: 9px;
+            display: inline-block;
+            background-repeat: no-repeat;
+            width:  335px;
+            height: 43px;
+            position: absolute;
+        }
+        
+        #email{
+            background-color: transparent;
+            color: white;
+            font-size: 15pt;
+            border: none;
+            width: 320px;
+            height: 25px;
+        }
+
+        #email:focus{
+            outline: 0;
+        }
+
+        #formContainer{
+            left: 50%;
+            top: 50%;
+            position: absolute;
+            width: 500px;
+            height: 80px;
+            margin-left: -250px;
+            margin-top: -40px;
+        }
+
+        #submitButton{
+            color: #5dc6a5;
+            font-size: 11pt;
+            background-image: url("/img/roundedButton.png");
+            width: 135px;
+            height: 43px;
+            border: none;
+            background-color: transparent;
+            position: absolute;
+            left: 350px;
+        }
+    </style>
+
     <script type="text/javascript">
         var isValid = false;
         var errorText = "На такой адрес приглашение не придет :)";
@@ -20,16 +113,16 @@
                 var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
                 isValid = pattern.test(value);
                 if(isValid){
-                    email.css({'border' : '1px solid #569b44'});
+                    //email.css({'border' : '1px solid #569b44'});
                     $('#valid').text('');
                 }
                 if(!isValid){
-                    $(this).css({'border' : '1px solid #ff0000'});
+                    //$(this).css({'border' : '1px solid #ff0000'});
                     $('#valid').text(errorText);
                 }
             } else {
                 isValid = false;
-                email.css({'border' : '1px solid #ff0000'});
+                //email.css({'border' : '1px solid #ff0000'});
                 $('#valid').text(errorText);
             }
         }
@@ -45,17 +138,14 @@
     </script>
 </head>
 <body>
-<!--    test-->
+    <div id="first">
+
     <?php
-
-    echo 3 + 4 + 4 + 3 + 4 + 3 + 4 + 4+ 4+4+4+4+3+2+4+3+4+4+3+3+4+3+3+4+4;
-
-
 
     if (isset( $_POST['email']) && $_POST['email'] != '')
     {
 
-        echo "molodets!";
+    echo "molodets!";
 
     $con=mysqli_connect("localhost","root","root","nogaDB");
 
@@ -77,22 +167,29 @@
     else
     {
         ?>
-
+        <div id="formContainer">
         <form name ="form1" method="post" action="index.php">
-            <p>
-                <input id="email" type="email" value="" name="email" required="required"><span id="valid"></span>
-            </p>
-            <p>
-                <input id="submitButton" type= "Submit" Name = "Submit1" VALUE = "Podpisat`s`a">
-            </p>
+            <div>
+                <div id="emailDiv">
+                    <input id="email" value="" name="email" placeholder="Your@mail.com">
+                </div>
+
+                <input id="submitButton" type= "Submit" Name = "Submit1" VALUE = "Подписаться">
+
+                <p><span id="valid"></span></p>
+            </div>
         </form>
+        </div>
 
     <?php
-        echo "StringEmpty";
+
     }
 
     ?>
-
+    </div>
+    <div id="second">
+        <img src="/img/labyrinth.png" />
+    </div>
 </body>
 </html>
 
