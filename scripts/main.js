3508df570ff591b28f28a18a05d135fca7e30096
felicita <alex.stepanov@me.com>
2013-09-25 18:46:23 +0300

    refactoring

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var isValid = false;
+var errorText = "На такой адрес приглашение не придет :)";
+
+function validate(){
+    var email = $('#email');
+    var value = email.val();
+    if(value != '') {
+        var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+        isValid = pattern.test(value);
+        if(isValid){
+            //email.css({'border' : '1px solid #569b44'});
+            $('#valid').text('');
+        }
+        if(!isValid){
+            //$(this).css({'border' : '1px solid #ff0000'});
+            $('#valid').text(errorText);
+        }
+    } else {
+        isValid = false;
+        //email.css({'border' : '1px solid #ff0000'});
+        $('#valid').text(errorText);
+    }
+}
+
+$(document).ready(function() {
+    $('#email').blur(validate);
+
+    $('#submitButton').click(function(){
+        validate();
+        return isValid;
+    });
+});
